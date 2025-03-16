0b7c45d0218fbae0c3c03d6ddd1529bcc9d0c335
Davit Gasparyan <dgasparyan073@gmail.com>
2025-03-16 20:45:42 +0400

    Added ClimateNet API and table to display its data

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    data = None
+    if request.method == "POST":
+        city = request.form.get("city")
+        match city:
+            case "Maralik":
+                city_id = 1
+            case "Panik":
+                city_id = 2
+            case "Azatan":
+                city_id = 3
+            case "Artik":
+                city_id = 4
+            case "Yerevan":
+                city_id = 8
+            case "Sevan":
+                city_id = 9
+            case "Gavar":
+                city_id = 6
+        response = requests.get(f"https://emvnh9buoh.execute-api.us-east-1.amazonaws.com/getData?device_id={city_id}")
+        if response.status_code == 200:
+            json_data = response.json()
+
+            keys = json_data['keys']
+            first_data_element = json_data['data'][0]
+            data = {}
+
+            for key, value in zip(keys, first_data_element):
+                data[key] = value
+            return jsonify(data)
+        else:
+            return jsonify({"error": "Failed to fetch data"}), 400
+    return render_template("home.html", data=data)
 
 @app.route("/about")
 def about():
