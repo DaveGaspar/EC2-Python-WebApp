189ac2523c09f4cbbc4649921848313aa66be0d5
Davit Gasparyan <dgasparyan073@gmail.com>
2025-03-16 23:47:45 +0400

    Added endpoint to get city data

--- main.py
+++ main.py
@@ -38,6 +38,41 @@ def home():
             return jsonify({"error": "Failed to fetch data"}), 400
     return render_template("home.html", data=data)
 
+@app.route('/city/<city_name>')
+def get_city(city_name):
+    city = city_name
+    data = None
+    city_id = None
+    match city:
+        case "Maralik":
+            city_id = 1
+        case "Panik":
+            city_id = 2
+        case "Azatan":
+            city_id = 3
+        case "Artik":
+            city_id = 4
+        case "Yerevan":
+            city_id = 8
+        case "Sevan":
+            city_id = 9
+        case "Gavar":
+            city_id = 6
+    if not city_id:
+        return jsonify({"error": "City not found"}), 404
+    response = requests.get(f"https://emvnh9buoh.execute-api.us-east-1.amazonaws.com/getData?device_id={city_id}")
+    if response.status_code == 200:
+        json_data = response.json()
+
+        keys = json_data['keys']
+        data = json_data['data']
+
+        zipped_data = list(zip(keys, data))
+
+        return render_template('city.html', city_name=city_name, zipped_data=zipped_data)
+    else:
+        return jsonify({"error": "Failed to fetch data"}), 400
+
 @app.route("/about")
 def about():
     return render_template("about.html")
